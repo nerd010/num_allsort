9993834595eef5d5539adcfe2f21be49d75b7e8d
Charles Wang <Charles@wangzengkangdemacbook-pro.local>
2016-03-24 16:22:07 +0800

    Initial Commit

---
+++ 数的全排列/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  数的全排列
+//
+//  Created by Charles Wang on 16/3/24.
+//  Copyright © 2016年 CHW. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
